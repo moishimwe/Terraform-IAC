2089ebf7c6d431791d9202716fd884e7685edafa
modeste <moishimwe@gmail.com>
2023-12-17 16:12:01 -0700

    testing

--- 1-Create EC2/ec2.tf
+++ 1-Create EC2/ec2.tf
@@ -10,7 +10,7 @@ resource "aws_instance" "test" {
 
 # Below is the provider which helps in connecting with AWS Account
 provider "aws" {
-  region = ""
+  region = "us-east-1"
   access_key = ""
   secret_key = ""
 }
